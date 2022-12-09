d79cc3fb7de005fafcb83056d4cd0f4bff23acfa
mohamedabdi <mohamed.abdi11@hotmail.co.uk>
2022-12-09 11:52:02 +0000

    removed the port in order to deploy

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import morgan from "morgan"
+import morgan from "morgan";
 
 //import routes
 import { expensesRouter } from "./Routes/expenses.js";
@@ -7,18 +7,16 @@ import { expensesRouter } from "./Routes/expenses.js";
 import cors from "cors";
 
 export const app = express();
-const PORT = process.env.port || 3000;
 
 app.use(cors());
 app.use(morgan("dev"));
 //read static files
-app.use(express.static("public"))
-app.use(express.json())
+app.use(express.static("public"));
+app.use(express.json());
 
 //middleware for routes
 app.use("/api/expenses", expensesRouter);
 
-
-app.listen(PORT, function (){
-    console.log(`Server is running on port ${PORT}`)
-});
+// app.listen(PORT, function (){
+//     console.log(`Server is running on port ${PORT}`)
+// });
