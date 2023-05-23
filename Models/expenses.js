577b4c1e250d0bf739bbaf5cbaddec14d0a8bbab
Mohamed Abdi <mohamed.abdi11@hotmail.co.uk>
2023-05-23 21:30:10 +0100

    New column added to Route

--- Models/expenses.js
+++ Models/expenses.js
@@ -1,13 +1,16 @@
 import query from "../db/index.js";
 
-export async function getAllExpenses(){
-    const result = await query("SELECT * FROM expenses");
-    const expenses = result.rows 
-    return expenses
+export async function getAllExpenses() {
+  const result = await query("SELECT * FROM expenses");
+  const expenses = result.rows;
+  return expenses;
 }
 
-export async function createExpense(expenses){
-const {expense, price, date} = expenses;
-const result = await query(`INSERT INTO expenses (expense, price, date) VALUES ($1, $2, $3) RETURNING *`, [expense, price, date]);
-return result.rows;
+export async function createExpense(expenses) {
+  const { category, expense, price, date } = expenses;
+  const result = await query(
+    `INSERT INTO expenses (expense, price, date) VALUES ($1, $2, $3, $4) RETURNING *`,
+    [category, expense, price, date]
+  );
+  return result.rows;
 }
