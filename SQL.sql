614252179117e1a7da3c2a37ad8e9d1705c96baf
Mohamed Abdi <mohamed.abdi11@hotmail.co.uk>
2023-05-23 21:29:07 +0100

    new schema added

--- SQL.sql
+++ SQL.sql
@@ -13,22 +13,22 @@ VALUES
 CREATE TABLE expenses(
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id INTEGER REFERENCES users(id),
+    category TEXT,
     expense  TEXT,
     price VARCHAR(30),
     date  DATE
 );
 
 INSERT INTO expenses
-      (user_id, expense, price, date)
+      (user_id, category, expense, price, date)
 VALUES
-     (1,'Netflix', '10.99', '11/1/2022'),
-     (1,'Gym', '25.99','11/4/2022'),
-     (1,'Amazon Prime', '8.99', '11/1/2022'),
-     (1,'Groceries', '200','11/29/2022'),
-     (1,'Rent', '1100', '11/1/2022'),
-     (1,'Electricity', '130','11/27/2022'),
-     (1,'Council Tax', '180', '11/1/2022'),
-     (1,'Water', '60','11/4/2022'),
-     (1,'Uber Eats', '40', '11/1/2022');
-     
+     (1, 'Entertainment', 'Netflix', '10.99', '11/1/2022'),
+     (1, 'Health/Fitness', 'Gym', '25.99','11/4/2022'),
+     (1,'Entertainment','Amazon Prime', '8.99', '11/1/2022'),
+     (1,'Food','Groceries', '200','11/29/2022'),
+     (1,'Housing/ Utilities','Rent', '1100', '11/1/2022'),
+     (1,'Housing/ Utilities','Electricity', '130','11/27/2022'),
+     (1,'Housing/ Utilities','Council Tax', '180', '11/1/2022'),
+     (1,'Housing/ Utilities','Water', '60','11/4/2022'),
+     (1,'Food','Uber Eats', '40', '11/1/2022');
      
